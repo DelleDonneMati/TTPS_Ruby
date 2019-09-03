958ba630a4ce3a201eac38cd38f6a358d9acd5da
DelleDonneMati <matiasdeledone@hotmail.com.ar>
2019-09-03 11:41:21 -0300

    Practica 1

--- Practica_1/Ejercicio4.rb
+++ Practica_1/Ejercicio4.rb
@@ -7,22 +7,26 @@
 #si el minuto está entre 45 y 55, debe decir "menos cuarto" (de la hora siguiente),
 #y si el minuto está entre 56 y 59, debe decir "casi las" (y la hora siguiente)
 
-def hora_Palabras(unMinuto)
-	case unMinuto
-	when 0
-	  puts "You ran out of gas."
-	when 1...20
-	  p "The tank is almost empty. Quickly, find a gas station!"
-	when 21...70
-	  "You should be ok for now."
-	when 71...100
-	  "The tank is almost full."
+def  en_palabras()
+	t = Time.now
+	case t.min
+	when 0...10
+		puts "En punto"
+	when 11...20
+		puts "y cuarto"	
+	when 21...34
+		puts "y media"
+	when 35...44
+		a = t.hour + 1
+		puts "#{a} menos veinticinco "
+	when 45...55
+		puts "#{a} menos cuarto"
+	when 56...59
+		puts "#{a} casi las"
 	else
-	  "Error"
+		puts "HOLA"		
 	end
-
 end
-	p t = Time.now
-	a = t.strftime("%M")
-	hora_Palabras(a)
-	puts "hola"
+
+
+en_palabras()
