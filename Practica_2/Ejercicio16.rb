cd8010ef4ef322a9e9a3f096511d11a17f6abbdb
DelleDonneMati <matiasdeledone@hotmail.com.ar>
2019-09-17 15:23:32 -0300

    Practica 2

---
+++ Practica_2/Ejercicio16.rb
@@ -0,0 +1,17 @@
+# 16. Escribí un método da_nil? que reciba un bloque, lo invoque y retorne si el valor de retorno del bloque fue nil . Por ejemplo:
+# da_nil? { }
+# => true
+# da_nil? do
+# 'Algo distinto de nil'
+# end
+# => false
+
+
+def da_nil?
+	yield.nil?
+end
+
+p da_nil? {}
+
+
+p da_nil? {'Hola'}
